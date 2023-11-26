ff1797fe4071ba1cb4d86a58997df2ceea54b33e
jasonchee816 <84338575+jasonchee816@users.noreply.github.com>
2023-11-26 21:51:05 +0800

    Initial Commit

---
+++ iOSBarcodeScanner/iOSBarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSBarcodeScannerApp.swift
+//  iOSBarcodeScanner
+//
+//  Created by Axflix on 26/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct iOSBarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iOSBarcodeScannerUITests/iOSBarcodeScannerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  iOSBarcodeScannerUITestsLaunchTests.swift
+//  iOSBarcodeScannerUITests
+//
+//  Created by Axflix on 26/11/2023.
+//
+
+import XCTest
+
+final class iOSBarcodeScannerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
